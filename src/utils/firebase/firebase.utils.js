aa12f343942d36fcb2137e0252483a68e21d880c
Ali Farooq <alifarooq122@gmail.com>
2023-06-15 02:28:40 -0700

    added useful comments to firebase util file

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,3 +1,4 @@
+// Import necessary functions and modules from the Firebase SDK
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -8,6 +9,7 @@ import {
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
+// Configuration object containing Firebase project details
 const firebaseConfig = {
   apiKey: "AIzaSyAxPBLcEQRyyD9iA1-TY6PCWKtpziLQzjc",
   authDomain: "crwn-clothing-db-f7887.firebaseapp.com",
@@ -17,27 +19,37 @@ const firebaseConfig = {
   appId: "1:124046210430:web:d9407a18609b5b8f486f2f",
 };
 
+// Initialize Firebase app with the provided configuration
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Create a new instance of GoogleAuthProvider for Google sign-in
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
 
+// Get the authentication service instance
 export const auth = getAuth();
+
+// Function to sign in with Google using a popup window
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+// Get the Firestore database service instance
 export const db = getFirestore();
 
+// Function to create a user document in the Firestore database
 export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, "users", userAuth.uid);
-  const userSnapshot = await getDoc(userDocRef);
+  const userDocRef = doc(db, "users", userAuth.uid); // Reference to the user document in the "users" collection
 
+  // Check if the user document already exists
+  const userSnapshot = await getDoc(userDocRef);
   if (!userSnapshot.exists()) {
-    const { displayName, email } = userAuth;
+    // If the user document doesn't exist, create it
+    const { displayName, email } = userAuth; // Extract user information from the authentication object
     const createdAt = new Date();
 
     try {
+      // Set the user document with the provided data
       await setDoc(userDocRef, {
         displayName,
         email,
@@ -48,5 +60,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
   }
 
+  // Return the reference to the user document
   return userDocRef;
 };
